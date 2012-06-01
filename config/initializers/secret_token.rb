86efd0f447bb456631e22c386629552ea98e171e
Ryan Romanchuk <rromanchuk@gmail.com>
2012-06-02 00:22:14 +0400

    stub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ryanromanchuk::Application.config.secret_token = 'e616631b1ccf074651d547c273d99b005980c5f460a49a32d1812fe93ab54006f0547df29974f3f382d2c0722c601ed71a1116e5fbbf7d723a4f307c642a6bd4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ryanromanchuk::Application.config.session_store :cookie_store, key: '_ryanromanchuk_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ryanromanchuk::Application.config.session_store :active_record_store
